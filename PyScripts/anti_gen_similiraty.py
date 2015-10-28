0c95913292665e485ae557e14a5e7721c8db96c1
Piotr Bentkowski <bentkowski.piotr@gmail.com>
2015-10-28 19:30:53 +0100

    Script to analyze pathogen spp richness

--- PyScripts/anti_gen_similiraty.py
+++ PyScripts/anti_gen_similiraty.py
@@ -9,8 +9,35 @@ for Evolutionary Biology Group, Faculty of Biology
 @author: Piotr Bentkowski - bentkowski.piotr@gmail.com
 """
 import sys
+import re
 from bitstring import BitArray
 
 
 def loadThePopulation(FILE):
-    while
+    ''' '''
+    LL = []
+    spp_flag = False
+    try:
+        with open(FILE) as infile:
+            for line in infile:
+                if re.search(r"===", line):
+                    ll = []
+                    spp_flag = True
+                    print line,
+                elif spp_flag:
+
+    except IOError as e:
+        print "I/O error({0}) in loadThePopulation(): {1}".format(e.errno,
+                                                                  e.strerror)
+
+
+def main():
+    if len(sys.argv) <= 1:
+        print "Give the name of the file with data."
+        sys.exit()
+    loadThePopulation(sys.argv[1])
+    print "DONE!"
+
+
+if __name__ == "__main__":
+    main()
