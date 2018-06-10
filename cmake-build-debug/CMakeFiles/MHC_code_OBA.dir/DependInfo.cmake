7b94398757aa90c6c1633951121a108e3d66e89b
Piotr Bentkowski <bentkowski.piotr@gmail.com>
2018-06-11 00:20:43 +0200

    Minor changes in IDE settings

--- cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/DependInfo.cmake
@@ -4,16 +4,16 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/piotr/MHC_code/Antigen.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Antigen.cpp.o"
-  "/home/piotr/MHC_code/DataHandler.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/DataHandler.cpp.o"
-  "/home/piotr/MHC_code/Environment.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Environment.cpp.o"
-  "/home/piotr/MHC_code/Gene.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Gene.cpp.o"
-  "/home/piotr/MHC_code/H2Pinteraction.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/H2Pinteraction.cpp.o"
-  "/home/piotr/MHC_code/Host.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Host.cpp.o"
-  "/home/piotr/MHC_code/Pathogen.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Pathogen.cpp.o"
-  "/home/piotr/MHC_code/RandomNumbs.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/RandomNumbs.cpp.o"
-  "/home/piotr/MHC_code/Tagging_system.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Tagging_system.cpp.o"
-  "/home/piotr/MHC_code/main.cpp" "/home/piotr/MHC_code/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/main.cpp.o"
+  "/home/piotr/MHCevo/Antigen.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Antigen.cpp.o"
+  "/home/piotr/MHCevo/DataHandler.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/DataHandler.cpp.o"
+  "/home/piotr/MHCevo/Environment.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Environment.cpp.o"
+  "/home/piotr/MHCevo/Gene.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Gene.cpp.o"
+  "/home/piotr/MHCevo/H2Pinteraction.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/H2Pinteraction.cpp.o"
+  "/home/piotr/MHCevo/Host.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Host.cpp.o"
+  "/home/piotr/MHCevo/Pathogen.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Pathogen.cpp.o"
+  "/home/piotr/MHCevo/RandomNumbs.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/RandomNumbs.cpp.o"
+  "/home/piotr/MHCevo/Tagging_system.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/Tagging_system.cpp.o"
+  "/home/piotr/MHCevo/main.cpp" "/home/piotr/MHCevo/cmake-build-debug/CMakeFiles/MHC_code_OBA.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
