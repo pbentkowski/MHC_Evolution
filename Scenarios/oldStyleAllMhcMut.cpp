a7d280eb36d3120eb772d1703afe522f9730422a
Piotr Bentkowski <pbentkowski@users.noreply.github.com>
2019-02-04 12:57:03 +0100

    Update oldStyleAllMhcMut.cpp

--- Scenarios/oldStyleAllMhcMut.cpp
+++ Scenarios/oldStyleAllMhcMut.cpp
@@ -42,7 +42,7 @@ using jsonf = nlohmann::json;
  */
 void printTipsToRun(){
     std::cout << std::endl;
-    std::cout << "This is the a scenario with NO sexual selection. The hosts MHC mutation are a 'whole-gene mutation' (instead of point mutations)."
+    std::cout << "This is a scenario with NO sexual selection. The hosts MHC mutation are a 'whole-gene mutation' (instead of point mutations)."
             " Parameters should be:" << std::endl;
     std::cout << " 1. The number of threads the program will use. Give 0 to use all the available CPU cores." << std::endl;
     std::cout << " 2. Number of bits in a MHC gene." << std::endl;
