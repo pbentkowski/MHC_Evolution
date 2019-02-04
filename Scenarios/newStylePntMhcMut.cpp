42cef64929579144979cbc35889b0cfed4a89b25
Piotr Bentkowski <pbentkowski@users.noreply.github.com>
2019-02-04 12:55:59 +0100

    Update newStylePntMhcMut.cpp

--- Scenarios/newStylePntMhcMut.cpp
+++ Scenarios/newStylePntMhcMut.cpp
@@ -42,7 +42,7 @@ using jsonf = nlohmann::json;
  */
 void printTipsToRun(){
     std::cout << std::endl;
-    std::cout << "This is the a no sexual selection scenario. The hosts MHC mutation are point mutations."
+    std::cout << "This is a scenario with NO sexual selection. The hosts MHC mutations are point mutations."
             " Parameters should be:" << std::endl;
     std::cout << " 1. The number of threads the program will use. Give 0 to use all the available CPU cores." << std::endl;
     std::cout << " 2. Number of bits in a MHC gene." << std::endl;
